3020c27d44f64bc3b8f198e2abe177b00bee27c9
Mahdi Abbastabaar <abbastabar.mahdi1381@gmail.com>
2024-05-12 19:07:34 +0330

    first commit

---
+++ src/main/java/com/example/CountriesData/CountriesDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.CountriesData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountriesDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CountriesData/CountriesDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CountriesData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
