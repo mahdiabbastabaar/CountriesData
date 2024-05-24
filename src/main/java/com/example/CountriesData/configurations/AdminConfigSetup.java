3d3e337e686dd04ab2b3dea5603c54710ecdb028
yasminkadkhodaei <59218135+yasminkadkhodaei@users.noreply.github.com>
2024-05-24 19:20:21 +0330

    Adding admin configurations

---
+++ src/main/java/com/example/CountriesData/configurations/AdminConfigSetup.java
@@ -0,0 +1,26 @@
+package com.example.CountriesData.configurations;
+
+import com.example.CountriesData.dto.auth.RegisterRequestDto;
+import com.example.CountriesData.models.user.Role;
+import com.example.CountriesData.services.AuthenticationService;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class AdminConfigSetup {
+
+    private final AdminConfiguration adminConfiguration;
+    private final AuthenticationService authenticationService;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void setupAdminConfigs() throws Exception {
+        RegisterRequestDto requestDto =
+            new RegisterRequestDto(adminConfiguration.getUsername(), adminConfiguration.getPassword());
+        //TODO handle exception later
+        authenticationService.register(requestDto, Role.ADMIN);
+
+    }
+}
