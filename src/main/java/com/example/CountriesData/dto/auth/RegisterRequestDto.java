0f4a63e0183ec2aed5e4af375e0ed3862da790d8
Mahdi Abbastabaar <abbastabar.mahdi1381@gmail.com>
2024-06-30 01:59:02 +0330

    feat: added submodules and front directory to project

--- src/main/java/com/example/CountriesData/dto/auth/RegisterRequestDto.java
+++ src/main/java/com/example/CountriesData/dto/auth/RegisterRequestDto.java
@@ -6,13 +6,11 @@ import jakarta.validation.constraints.Pattern;
 
 /**
  * Request body of register request.
-
  */
 public record RegisterRequestDto(
     @NotNull @Pattern(regexp = User.USERNAME_REGEX) String username,
     @NotNull @Pattern(regexp = User.PASSWORD_REGEX) String password
 ) {
-
     public RegisterRequestDto(
         String username,
         String password
