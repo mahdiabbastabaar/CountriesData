7d4670bb04498eb5c8e0acb0517f02ffdb964dbd
Mahdi Abbastabaar <abbastabar.mahdi1381@gmail.com>
2024-07-07 17:31:42 +0330

    fix: hotfix

--- UserManagementService/src/main/java/org/example/configurations/AdminConfigSetup.java
+++ UserManagementService/src/main/java/org/example/configurations/AdminConfigSetup.java
@@ -2,8 +2,8 @@ package org.example.configurations;
 
 import org.example.dto.auth.RegisterRequestDto;
 import org.example.models.Role;
-import org.example.services.AuthenticationService;
 import lombok.AllArgsConstructor;
+import org.example.services.AuthenticationService;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
